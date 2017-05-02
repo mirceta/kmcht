bc1bca6014e32e3168801df50a48da772ea8577c
kristijan.mirceta <kristijan.mirceta@gmail.com>
2017-05-02 15:46:41 +0200

    Select, insert methods for cheatsheet, knowledge piece are done in node.js

--- backend/migrations.js
+++ backend/migrations.js
@@ -11,16 +11,16 @@ exports.createDatabase = function(conn) {
 
 	  IF OBJECT_ID('dbo.Cheatsheet', 'U') IS NULL
 		CREATE TABLE Cheatsheet (
-			PK_CheatsheetId uniqueidentifier primary key,
+			PK_CheatsheetId int not null identity(1,1) primary key,
 			Lang varchar(50)
 		)
 
 	  IF OBJECT_ID('dbo.KnowledgePiece', 'U') IS NULL
 		CREATE TABLE KnowledgePiece (
-			PK_id uniqueidentifier primary key,
+			PK_id int not null identity(1,1) primary key,
 			FK_CheatsheetId int not null,
-			CodeDescription varchar,
-			Code varchar
+			CodeDescription varchar(MAX),
+			Code varchar(MAX)
 		)
 	`;
 	var request = new ted.Request(sql, function(err, rowCount) {
@@ -34,58 +34,97 @@ exports.createDatabase = function(conn) {
 }
 
 // exists ops
-exports.existsCheatsheet = function(conn) {
+exports.existsCheatsheet = function(conn, lang, exists, notexists) {
 	var sql = `
-		SELECT TOP 1 [PK_id]
-		FROM [kmcht_test].[dbo].[KnowledgePiece]
+		SELECT [PK_CheatsheetId], [Lang]
+		FROM [kmcht_test].[dbo].[Cheatsheet]
+		WHERE [Lang] = '` + lang + `' 
 	`;
 	var request = new ted.Request(sql, function(err, rowCount) {
 		if (err) { console.log(err); }
 		else {
-			if (rowCount > 0) {
-				console.log("Some exist, yes");
-			} else {
-				console.log("No man, sadly");
-			}
 			
 		}
 	});
+	
+	var ext = null;
+	request.on('row', function(columns) {
+    columns.forEach(function(column) {
+      if (column.value == lang) {
+        ext = columns[0].value;
+      } 
+    });
+  });
+
+	request.on('requestCompleted', function() {
+		if (ext != null) {
+			console.log('ExistsCheatsheet done: calling exists');
+			exists(ext);
+		} else {
+			console.log('ExistsCheatsheet done: calling notexists');
+			notexists();
+		}
+	});
+
 	conn.execSql(request);
 }
 
 // insert ops
-exports.insertCheatsheet = function(conn) {
+exports.insertCheatsheet = function(conn, lang, then) {
 	var sql = `
-		USE [kmcht_test]
-		GO
+		USE [kmcht_test]		
 
 		INSERT INTO [dbo].[Cheatsheet]
-							([PK_CheatsheetId]
-							,[Lang])
-				VALUES
-							(<PK_CheatsheetId, uniqueidentifier,>
-							,<Lang, varchar(50),>)
-		GO
+							([Lang])
+		VALUES
+							('` + lang + `')
+		select scope_identity()
 	`;
+	var request = new ted.Request(sql, function(err, rowCount) {
+		if (err) { console.log(err); }
+		else {
+		}
+	});
+
+	var data = null;
+	request.on('row', function(columns) {
+    columns.forEach(function(column) {
+      data = column.value;
+    });
+  });
+	request.on('requestCompleted', function() {
+		if (data != null) {
+			console.log('InsertCheatsheet done: calling then(' + data + ')');
+			then(data);
+		}
+	});
+
+	conn.execSql(request);
 }
 
-exports.insertKnowledgePiece = function(conn) {
-	var sql = `
+exports.insertKnowledgePiece = function(conn, langid, code, desc) {
+	var insrt = function() {
+		var sql = `
 		USE [kmcht_test]
-		GO
 
 		INSERT INTO [dbo].[KnowledgePiece]
-							([PK_id]
-							,[FK_CheatsheetId]
+							([FK_CheatsheetId]
 							,[CodeDescription]
 							,[Code])
 				VALUES
-							(<PK_id, uniqueidentifier,>
-							,<FK_CheatsheetId, int,>
-							,<CodeDescription, varchar(1),>
-							,<Code, varchar(1),>)
-		GO
-	`;
+							(` + langid + `
+							,'` + code + `'
+							,'` + desc + `')
+		`;
+		var request = new ted.Request(sql, function(err, rowCount) {
+			if (err) { console.log(err); }
+			else {
+				console.log('Inserted Knowledge piece');
+			}
+		});
+		conn.execSql(request);
+	};
+	insrt();
 }
 
 // update ops
@@ -141,28 +180,73 @@ exports.deleteKnowledgePiece = function(conn) {
 }
 
 // select ops
-exports.selectCheatsheet = function(conn) {
+exports.selectCheatsheet = function(conn, lang, then) {
 	var sql = `
 		USE [kmcht_test]
-		GO
 
 		SELECT [PK_CheatsheetId]
 					,[Lang]
-			FROM [dbo].[Cheatsheet]
-		GO
-	`;
+			FROM [dbo].[Cheatsheet] `;
+		/*if (lang != null) 
+		  sql += "WHERE [Lang] = '" + lang + "' ";
+		sql += ` `;*/
+	var request = new ted.Request(sql, function(err, rowCount) {
+			if (err) { console.log(err); }
+	});
+
+	data = [];
+	request.on('row', function(columns) {
+
+		dat = {};
+    columns.forEach(function(column) {
+      dat[column.metadata.colName] = column.value;
+    });
+		data.push(dat);
+
+  });
+
+	request.on('requestCompleted', function() {
+		if (data != null) {
+			console.log('returning ' + data.length + ' rows');
+			then(data);
+		}
+	});
+
+	conn.execSql(request);
 }
 
-exports.selectKnowledgePiece = function(conn) {
+exports.selectKnowledgePiece = function(conn, then) {
 	var sql = `
 		USE [kmcht_test]
-		GO
 
 		SELECT [PK_id]
-					,[FK_CheatsheetId]
+					,c.[Lang]
 					,[CodeDescription]
 					,[Code]
-			FROM [dbo].[KnowledgePiece]
-		GO
+		FROM [dbo].[KnowledgePiece] kp, [dbo].[Cheatsheet] c
+		WHERE kp.FK_CheatsheetId = c.PK_CheatsheetId
 	`;
+	var request = new ted.Request(sql, function(err, rowCount) {
+			if (err) { console.log(err); }
+	});
+
+	data = [];
+	request.on('row', function(columns) {
+		
+		dat = {};
+    columns.forEach(function(column) {
+      dat[column.metadata.colName] = column.value;
+    });
+		data.push(dat);
+
+  });
+
+	request.on('requestCompleted', function() {
+		if (data != null) {
+			console.log('returning ' + data.length + ' rows');
+			then(data);
+		}
+	});
+
+	conn.execSql(request);
 }
--- backend/service.js
+++ backend/service.js
@@ -1,5 +1,7 @@
 var ted = require('tedious');
 var mig = require('./migrations');
+var express = require('express');
+var app = express();
 
 console.log('required tedious');
 
@@ -15,13 +17,106 @@ var config = {
 	}
 }
 
+// REST
+app.get('/insertKnowledge', function (req, res) {
+  // connect
+  var connection = new ted.Connection(config);
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successful');
+      var lang = req.query.lang;
+      var code = req.query.code;
+      var desc = req.query.desc;
+      mig.existsCheatsheet(connection, lang,
+                                  function(id) {
+                                    mig.insertKnowledgePiece(connection, 
+                                              id, code, desc);
+                                  }, 
+                                  function() {
+                                    mig.insertCheatsheet(connection, lang,
+                                      function(id) {
+                                        mig.insertKnowledgePiece(connection,
+                                          id, code, desc);
+                                      }
+                                    )
+                                  })
+    }
+  });
+})
 
-// connect
+app.get('/insertCheatsheet', function (req, res) {
+  // connect
+  var connection = new ted.Connection(config);
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successful');
+      var lang = req.query.lang;
+      mig.existsCheatsheet(connection, lang,
+                                  function(id) {
+                                    res.end('cheatsheet exists');
+                                  }, 
+                                  function() {
+                                    mig.insertCheatsheet(connection, lang,
+                                      function(id) {
+                                        res.end('successful');
+                                      }
+                                    )
+
+                                  })
+    }
+  });
+})
+
+app.get('/getCheatsheets', function (req, res) {
+  // connect
+  var connection = new ted.Connection(config);
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successful');
+      mig.selectCheatsheet(connection, '', function(data) {
+        res.end(JSON.stringify(data));
+      });
+    }
+  });
+})
+
+app.get('/getKnowledgePieces', function (req, res) {
+  // connect
+  var connection = new ted.Connection(config);
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successful');
+      mig.selectKnowledgePiece(connection, function(data) {
+        res.end(JSON.stringify(data));
+      });
+    }
+  });
+})
+
+// CREATE DATABASE AND LISTEN
 var connection = new ted.Connection(config);
-connection.on('connect', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('successful');
-  }
-});
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successful');
+      mig.createDatabase(connection);
+    }
+  });
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
